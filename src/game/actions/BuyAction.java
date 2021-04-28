54bd27849155ee992f31c45eb8a37e23d3a64095
JeremyYoonZheMin <jyoo0005@student.monash.edu>
2021-04-28 22:04:07 +0800

    Made BuyAction extend Action

--- src/game/actions/BuyAction.java
+++ src/game/actions/BuyAction.java
@@ -1,4 +1,17 @@
 package game.actions;
 
-public class BuyAction {
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class BuyAction extends Action {
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
 }
