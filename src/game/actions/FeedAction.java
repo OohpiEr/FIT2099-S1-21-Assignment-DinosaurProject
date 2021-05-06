7fe6566f0e94d16a4e6dc63a82a5daae10135803
JeremyYoonZheMin <jyoo0005@student.monash.edu>
2021-05-06 18:42:53 +0800

    Added Javadoc to FeedAction

--- src/game/actions/FeedAction.java
+++ src/game/actions/FeedAction.java
@@ -8,6 +8,9 @@ import game.Player;
 import game.dinosaurs.Dinosaur;
 import game.dinosaurs.Stegosaur;
 
+/**
+ * A class that lets an actor feed another actor a quantity of a food
+ */
 public class FeedAction extends Action {
 
     private final int ECO_POINT_REWARD = 10;
@@ -16,12 +19,24 @@ public class FeedAction extends Action {
     private Item food;
     private int quantity;
 
+    /**
+     * Constructor.
+     * @param target    The actor being fed the food
+     * @param food      The food being fed to the target
+     * @param quantity  The quantity of food being fed to the target
+     */
     public FeedAction(Actor target, Item food, int quantity){
         this.target = target;
         this.food = food;
         this.quantity = quantity;
     }
 
+    /**
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return  A description of the action
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         actor.removeItemFromInventory(food);
@@ -31,11 +46,16 @@ public class FeedAction extends Action {
         if (actor instanceof Player){
             Player.addEcoPoints(ECO_POINT_REWARD);
         }
-        return null;
+        return actor.toString() + " feeds " + quantity + " " + food.toString() + "(s) to " + target.toString();
     }
 
+    /**
+     * Returns a suitable menu description of the action
+     * @param actor The actor performing the action.
+     * @return  A suitable menu description of the action
+     */
     @Override
     public String menuDescription(Actor actor) {
-        return null;
+        return actor.toString() + " feeds " + quantity + " " + food.toString() + "(s) to " + target.toString();
     }
 }
