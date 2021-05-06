d78b5abe7457a65d2ef484588889e50a0be33fdd
JeremyYoonZheMin <jyoo0005@student.monash.edu>
2021-05-06 18:08:50 +0800

    Added and coded FeedAction

---
+++ src/game/actions/FeedAction.java
@@ -0,0 +1,41 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+import game.Player;
+import game.dinosaurs.Dinosaur;
+import game.dinosaurs.Stegosaur;
+
+public class FeedAction extends Action {
+
+    private final int ECO_POINT_REWARD = 10;
+
+    private Actor target;
+    private Item food;
+    private int quantity;
+
+    public FeedAction(Actor target, Item food, int quantity){
+        this.target = target;
+        this.food = food;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        actor.removeItemFromInventory(food);
+        if (target instanceof Dinosaur){
+            ((Dinosaur) target).eat(food, quantity);
+        }
+        if (actor instanceof Player){
+            ((Player) actor).addEcoPoints(ECO_POINT_REWARD);
+        }
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
