7e5975530fe81b79c48433d1e3bf5cddab97ae83
JeremyYoonZheMin <jyoo0005@student.monash.edu>
2021-05-12 22:58:28 +0800

    Added DrinkAction class

---
+++ src/game/actions/DrinkAction.java
@@ -0,0 +1,17 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class DrinkAction extends Action {
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
