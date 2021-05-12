c38bc3a5030e61ddeaffb187dff4f586576df6a5
JeremyYoonZheMin <jyoo0005@student.monash.edu>
2021-05-12 23:02:35 +0800

    Updated Javadoc

--- src/game/actions/DrinkAction.java
+++ src/game/actions/DrinkAction.java
@@ -4,6 +4,9 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * An action that causes an actor (currently only Dinosaurs) to drink from a lake
+ */
 public class DrinkAction extends Action {
     @Override
     public String execute(Actor actor, GameMap map) {
