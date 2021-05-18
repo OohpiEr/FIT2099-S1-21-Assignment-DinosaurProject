3e32a4a12d0cfac9deface60ea48e319935e13bd
ER_TIAN_RU <terr0001@student.monash.edu>
2021-05-18 13:13:49 +0800

    added javadoc comments to Teleporter

--- src/game/items/Teleporter.java
+++ src/game/items/Teleporter.java
@@ -6,41 +6,58 @@ import java.util.List;
 
 public class Teleporter extends Item {
 
-    Location location;
 
-    /***
+    /**
      * Constructor for non-invisible teleporter
-     *  @param name the name of this Item
+     *
+     * @param name        the name of this Item
+     * @param destination location to be teleported to
+     * @param direction   String describing the direction to move in, e.g. "North"
      */
-    public Teleporter(String name, GameMap gameMap) {
+    public Teleporter(String name, Location destination, String direction) {
         super(name, '^', false);
+        addTeleportAction(destination, direction);
     }
 
     /**
      * Constructor for invisible teleporter
      *
-     * @param name     the name of this Item
-
+     * @param name               the name of this Item
+     * @param teleporterLocation current teleporter location
+     * @param destination        location to be teleported to
+     * @param direction          String describing the direction to move in, e.g. "North"
      */
     public Teleporter(String name, Location teleporterLocation, Location destination, String direction) {
         super(name, '^', false);
         this.displayChar = getInvisibleDisplayChar(teleporterLocation);
-        this.location = teleporterLocation;
         addTeleportAction(destination, direction);
     }
 
+    /**
+     * Adds a MoveActorAction to teleporter's allowableActions
+     *
+     * @param destination location to be teleported to
+     * @param direction   String describing the direction to move in, e.g. "North"
+     */
     public void addTeleportAction(Location destination, String direction) {
         this.allowableActions.add(new MoveActorAction(destination, direction));
     }
 
-    private char getInvisibleDisplayChar(Location location) {
+    /**
+     * <p>Changes the teleporter's display char to match the display char of the ground
+     * or item on the ground (if there's an item) </>
+     *
+     * @param teleporterLocation current teleporter location
+     * @return a char to be displayed
+     */
+    private char getInvisibleDisplayChar(Location teleporterLocation) {
         char dispChar;
 
-        List<Item> items = location.getItems();
+        List<Item> items = teleporterLocation.getItems();
         if (items.size() != 0) {
             dispChar = items.get(items.size() - 1).getDisplayChar();
         } else {
-            dispChar = location.getGround().getDisplayChar();
+            dispChar = teleporterLocation.getGround().getDisplayChar();
         }
 
         return dispChar;
