bad42a62e12055ff459c44fc29cf359c1609cd08
ER_TIAN_RU <terr0001@student.monash.edu>
2021-05-24 15:26:57 +0800

    reformatted Player

--- src/game/Player.java
+++ src/game/Player.java
@@ -7,84 +7,89 @@ import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Menu;
 import game.actions.QuitGameAction;
+import game.grounds.Lake;
 
 /**
  * Class representing the Player.
  */
 public class Player extends Actor {
-	private static int ecoPoints = 0;
-	private int numberOfMoves;
-	private Game game;
-	private Menu menu = new Menu();
+    private static int ecoPoints = 0;
+    private int numberOfMoves;
+    private Game game;
+    private Menu menu = new Menu();
 
-	/**
-	 * Constructor.
-	 *
-	 * @param name        Name to call the player in the UI
-	 * @param displayChar Character to represent the player in the UI
-	 * @param hitPoints   Player's starting number of hitpoints
-	 * @param game		  Current game
-	 */
-	public Player(String name, char displayChar, int hitPoints, Game game) {
-		super(name, displayChar, hitPoints);
-		this.game = game;
-	}
+    /**
+     * Constructor.
+     *
+     * @param name        Name to call the player in the UI
+     * @param displayChar Character to represent the player in the UI
+     * @param hitPoints   Player's starting number of hitpoints
+     * @param game        Current game
+     */
+    public Player(String name, char displayChar, int hitPoints, Game game) {
+        super(name, displayChar, hitPoints);
+        this.game = game;
+    }
 
-	@Override
-	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
-		actions.add(new QuitGameAction(game));
-		game.tick(numberOfMoves, ecoPoints);
-		display.println(String.format("Eco Points: %d, Number of Moves: %d",  getEcoPoints(), getNumberOfMoves()));
-		numberOfMoves += 1;
-		// Handle multi-turn Actions
-		display.println("Eco Points: " + getEcoPoints());
-		if (Lake.getRainfall()!=0){
-			display.println("It's raining!");
-		}
-		if (lastAction.getNextAction() != null)
-			return lastAction.getNextAction();
-		return menu.showMenu(this, actions, display);
-	}
+    @Override
+    public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+        actions.add(new QuitGameAction(game));
+        game.tick(numberOfMoves, ecoPoints);
+        display.println(String.format("Eco Points: %d, Number of Moves: %d", getEcoPoints(), getNumberOfMoves()));
+        numberOfMoves += 1;
+        // Handle multi-turn Actions
+        display.println("Eco Points: " + getEcoPoints());
+        if (Lake.getRainfall() != 0) {
+            display.println("It's raining!");
+        }
+        if (lastAction.getNextAction() != null)
+            return lastAction.getNextAction();
+        return menu.showMenu(this, actions, display);
+    }
 
-	public int getNumberOfMoves() {
-		return numberOfMoves;
-	}
+    public int getNumberOfMoves() {
+        return numberOfMoves;
+    }
 
-	/**
-	 * Returns the player's number of eco points
-	 * @return	the player's number of eco points
-	 */
-	public static int getEcoPoints() {
-		return ecoPoints;
-	}
+    /**
+     * Returns the player's number of eco points
+     *
+     * @return the player's number of eco points
+     */
+    public static int getEcoPoints() {
+        return ecoPoints;
+    }
 
-	/**
-	 * Sets the player's number of eco points
-	 * @param ecoPoints	The new number of eco points the player should have
-	 */
-	public static void setEcoPoints(int ecoPoints) {
-		if(ecoPoints>=0){
-			Player.ecoPoints = ecoPoints;
-		}
-	}
+    /**
+     * Sets the player's number of eco points
+     *
+     * @param ecoPoints The new number of eco points the player should have
+     */
+    public static void setEcoPoints(int ecoPoints) {
+        if (ecoPoints >= 0) {
+            Player.ecoPoints = ecoPoints;
+        }
+    }
 
-	/**
-	 * Adds the provided number of eco points to the player's total eco points
-	 * @param ecoPoints	The number of eco points to be added to the player's total eco points
-	 */
-	public static void addEcoPoints(int ecoPoints){
-		if(ecoPoints>=0){
-			Player.ecoPoints+=ecoPoints;
-		}
-	}
+    /**
+     * Adds the provided number of eco points to the player's total eco points
+     *
+     * @param ecoPoints The number of eco points to be added to the player's total eco points
+     */
+    public static void addEcoPoints(int ecoPoints) {
+        if (ecoPoints >= 0) {
+            Player.ecoPoints += ecoPoints;
+        }
+    }
 
-	/**
-	 * Removes the provided number of eco points from the player's total eco points
-	 * @param ecoPoints	The number of eco points to be removed from the player's total eco points
-	 */
-	public static void removeEcoPoints(int ecoPoints){
-		if(ecoPoints>=0){
-			Player.ecoPoints-=ecoPoints;
-		}
-	}
+    /**
+     * Removes the provided number of eco points from the player's total eco points
+     *
+     * @param ecoPoints The number of eco points to be removed from the player's total eco points
+     */
+    public static void removeEcoPoints(int ecoPoints) {
+        if (ecoPoints >= 0) {
+            Player.ecoPoints -= ecoPoints;
+        }
+    }
 }
